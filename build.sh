e0d2a3b2259a2cb0c5127a0751b34b59100287bf
eric <eric@localhost.localdomain>
2018-08-21 16:42:15 +0800

    fix build on centos7

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+TARGET_OS=`uname -s`
+case "$TARGET_OS" in
+  Darwin)
+    export CGO_CFLAGS="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin"
+    ;;
+  Linux)
+    export CGO_CFLAGS="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux"
+    ;;
+  *)
+  echo "Unknown platform!" >&2
+  exit 1
+esac
+
 go get ./...
